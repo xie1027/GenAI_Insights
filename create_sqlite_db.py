2ba7ca505dfe4a46dd1081e79ee426ba367aebdb
davidmonaco <david.monaco@hotmail.com>
2023-10-15 13:26:52 -0400

    Added ACS data and cleaned up file

--- create_sqlite_db.py
+++ create_sqlite_db.py
@@ -4,8 +4,12 @@ Read in CSV files and create a SQLite database
 import sqlite3
 import pathlib
 import logging
+import datetime
 from pandas import DataFrame, read_csv
 from typing import Generator
+
+# Start time
+start_time: datetime.datetime = datetime.datetime.now()
 
 # Get the script name
 script_name: str = pathlib.Path(__file__).name.replace(".py", ".log")
@@ -14,7 +18,7 @@ script_name: str = pathlib.Path(__file__).name.replace(".py", ".log")
 logging.basicConfig(
     level=logging.INFO,
     format="%(asctime)s - %(levelname)s - %(message)s",
-    handlers=[logging.FileHandler(script_name), logging.StreamHandler()],
+    handlers=[logging.FileHandler(script_name, mode="w"), logging.StreamHandler()],
 )
 logger = logging.getLogger(__name__)
 logger.info(f"{script_name=}")
@@ -23,7 +27,7 @@ logger.info(f"{script_name=}")
 data_dir: pathlib.PosixPath = pathlib.Path.cwd() / "Data"
 
 # BEA data location
-bea_dir: pathlib.PosixPath = pathlib.Path.cwd() / "Data/bea_data"
+bea_dir: pathlib.PosixPath = data_dir / "bea_data"
 
 # Get the CSV files, which contain all states
 csv_files: Generator = bea_dir.glob("**/*ALL*.csv")
@@ -32,14 +36,10 @@ csv_files: Generator = bea_dir.glob("**/*ALL*.csv")
 dfs: dict[str, DataFrame] = {
     f.stem[: f.stem.index("__")].lower(): read_csv(f) for f in csv_files
 }
-# Check that all CSV files were read
-# logger.info(dfs.keys())
-# logger.info(len(dfs.keys()))
-# logger.info(len([f for f in bea_dir.glob("**/*ALL*.csv")]))
-
-# logger.info(dfs["sainc70"].info())
-# logger.info(dfs["sainc70"].head())
-# logger.info(dfs["sainc70"].tail())
+# Check that all CSV files were read into DataFrames
+logger.info(
+    f"All CSV files were read into DataFrames: {len(dfs.keys())==len([f for f in bea_dir.glob('**/*ALL*.csv')])}"
+)
 
 # Remove rows with missing values from all DataFrames
 dfs = {k: v.dropna(axis=0, how="any") for k, v in dfs.items()}
@@ -66,10 +66,6 @@ dfs = {
     for k, v in dfs.items()
 }
 
-# logger.info(dfs["sainc70"].info())
-# logger.info(dfs["sainc70"].head())
-# logger.info(dfs["sainc70"].tail())
-
 # Create permanent SQLite database in Data directory
 db = sqlite3.connect(data_dir / "data.sqlite3")  # ":memory:"
 
@@ -77,6 +73,11 @@ db = sqlite3.connect(data_dir / "data.sqlite3")  # ":memory:"
 for k, v in dfs.items():
     v.to_sql(name=k, con=db, if_exists="replace", index=False)
     logger.info(f"{k} loaded into database")
+
+# Load ACS data into SQLite database
+acs_df: DataFrame = read_csv(data_dir / "ACS_2012_21.csv")
+acs_df.to_sql(name="acs", con=db, if_exists="replace", index=False)
+logger.info(f"acs loaded into database")
 
 # List all tables in SQLite database
 cursor = db.cursor()
@@ -86,12 +87,10 @@ logger.info(cursor.fetchall())
 # Close database connection
 db.close()
 
-# Unique values in IndustryClassification column for all DataFrames
-# industries: dict[str, set[str]] = {
-#     k: set(v["IndustryClassification"].unique()) for k, v in dfs.items()
-# }
-# for k, v in industries.items():
-#     logger.info(f"{k=}")
-#     logger.info(f"{len(v)=}")
-#     logger.info(f"{v=}")
-#     logger.info()
+# End time
+end_time: datetime.datetime = datetime.datetime.now()
+
+# Elapsed time formatted
+elapsed_time: datetime.timedelta = str(end_time - start_time)
+
+logger.info(f"{script_name=} completed in {elapsed_time=}")
